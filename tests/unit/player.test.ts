5ce4459f62ceaa0867cf8756432ab44b40e8b460
francisco-magana <franciscomagana.contact@gmail.com>
2023-09-09 22:00:29 -0600

    add damage test

--- tests/unit/player.test.ts
+++ tests/unit/player.test.ts
@@ -1,12 +1,23 @@
+import { faker } from '@faker-js/faker';
 import { fakePlayer } from "../fake/player";
 
 describe('Player creation', () => {
 
     it('has valid data', () => {
-        expect(typeof(fakePlayer.name)).toBe("string");
-        expect(typeof(fakePlayer.life)).toBe("string");
-        expect(typeof(fakePlayer.stamina)).toBe("number");
-        expect(typeof(fakePlayer.mana)).toBe("number");
+        expect(typeof (fakePlayer.name)).toBe("string");
+        expect(typeof (fakePlayer.life)).toBe("number");
+        expect(typeof (fakePlayer.stamina)).toBe("number");
+        expect(typeof (fakePlayer.mana)).toBe("number");
+    })
+
+})
+
+describe('Player actions', () => {
+
+    it('receives damage', () => {
+        let initialHealth = fakePlayer.life;
+        let damage = faker.number.int({ min: 1, max: 10 });
+        expect(fakePlayer.takeDamage(damage)).toBe(initialHealth - damage)
     })
 
 })
