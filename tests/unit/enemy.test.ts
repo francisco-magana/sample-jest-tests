6e479c9ee2deb8c2e30c3a221366b023b5f2751a
francisco-magana <franciscomagana.contact@gmail.com>
2023-09-09 22:42:52 -0600

    add receive damage tests

--- tests/unit/enemy.test.ts
+++ tests/unit/enemy.test.ts
@@ -10,3 +10,35 @@ describe('Enemy creation', () => {
         expect(typeof (fakeEnemy.is_defeated)).toBe("boolean");
     })
 })
+
+describe('Enemy actions', () => {
+
+    let damage: number;
+    let full_life: number = 100;
+    let test_defense: number = 5;
+
+    beforeAll(() => {
+        damage = faker.number.int({min: 10, max: 20});
+        fakeEnemy.life = full_life;
+        fakeEnemy.defense = test_defense;
+    })
+  
+    it('receives damage', () => {
+        let expected_damage = fakeEnemy.life - (damage - fakeEnemy.defense);
+        fakeEnemy.takeDamage(damage);
+        expect(fakeEnemy.life).toBe(expected_damage);
+    })
+
+    it('calculate damage if is greather than defense', () => {
+        expect(fakeEnemy.calculateDamage(10)).toBe(5);
+    })
+
+    it('calculate damage if is less than defense', () => {
+        expect(fakeEnemy.calculateDamage(4)).toBe(0);
+    })
+
+    it('calculate damage if is equal to defense', () => {
+        expect(fakeEnemy.calculateDamage(test_defense)).toBe(0);
+    })
+
+})
