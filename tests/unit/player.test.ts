810f2a9174d8aa0928382ad2ca870bfa675aa4cd
francisco-magana <franciscomagana.contact@gmail.com>
2023-09-09 22:59:45 -0600

    add damage tests

--- tests/unit/player.test.ts
+++ tests/unit/player.test.ts
@@ -16,21 +16,35 @@ describe('Player actions', () => {
 
     let damage: number;
     let life_points: number;
+    let full_life: number = 100;
+    let test_defense: number = 5;
 
     beforeAll(() => {
-      damage = faker.number.int({ min: 1, max: 50 });
-      life_points = faker.number.int({ min: 1, max: 10 });
+      fakePlayer.defense = test_defense;
+      fakePlayer.life = full_life
     })
 
     it('receives damage', () => {
         let initialLife = fakePlayer.life;
-        expect(fakePlayer.takeDamage(damage)).toBe(initialLife - damage)
+        expect(fakePlayer.takeDamage(10)).toBe(initialLife - 5)
     })
 
     it('heal', () => {
         let initialLife = fakePlayer.life;
         fakePlayer.takeDamage(damage);
         expect(fakePlayer.heal(life_points)).toBe(initialLife - damage + life_points)
+    })
+
+    it('calculate damage if is greather than defense', () => {
+        expect(fakePlayer.calculateDamage(10)).toBe(5);
+    })
+
+    it('calculate damage if is less than defense', () => {
+        expect(fakePlayer.calculateDamage(4)).toBe(0);
+    })
+
+    it('calculate damage if is equal to defense', () => {
+        expect(fakePlayer.calculateDamage(test_defense)).toBe(0);
     })
 
 })
